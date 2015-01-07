b851a528799e68d67c92ceceb1056c4c036ea0f4
Walter Alejandro Camaly <waltercamaly@gmail.com>
2015-01-07 11:53:50 -0300

    Delete buttom

--- redditApp/src/main/java/Extras/Utils.java
+++ redditApp/src/main/java/Extras/Utils.java
@@ -8,32 +8,6 @@ import java.util.Date;
 
 public class Utils {
 
-	/*	private static VolleyServices volleys;
-	private static RequestQueue rq;
-	private static Bitmap thumbail;
-public static Bitmap LoadThumbail (String url, Context context){
-		volleys = VolleyServices.getInstance(context);
-		rq = volleys.getRequestQueue();
-
-			ImageRequest image = new ImageRequest(url, new Response.Listener<Bitmap>() {
-
-				@Override
-				public void onResponse(Bitmap imge) {
-					thumbail = imge;
-				}
-			}, 0, 0, null, new Response.ErrorListener() {
-
-				@Override
-				public void onErrorResponse(VolleyError arg0) {
-					thumbail = null;
-					
-				}
-			});
-		rq.add(image);
-		
-		return thumbail;
-	}
-	*/
 	
 	static public String ChangeDate (Long utc){
 	
